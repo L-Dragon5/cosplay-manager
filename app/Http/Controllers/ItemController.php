c312ccd74075711e5d19ed8b6ef63dde9ec46f02
L-Dragon5 <lildragon555@gmail.com>
2020-09-09 18:25:24 -0400

    Updating curl configuration for better scraping

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -420,17 +420,45 @@ class ItemController extends Controller
     }
 
     private function get_page($url) {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, True);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($curl, CURLOPT_ENCODING ,"");
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: text/plain; charset=UTF-8']); 
-        curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36');
-        $return = curl_exec($curl);
-        curl_close($curl);
-        return mb_convert_encoding($return, 'utf-8', 'gb2312');
+        $user_agent = [
+			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.7 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.7',
+			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/601.1.56 (KHTML, like Gecko) Version/9.0 Safari/601.1.56',
+			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0',
+			'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+			'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+			'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
+			'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+			'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/5.0)',
+        ];
+
+        $options = [
+			CURLOPT_RETURNTRANSFER => true, 	// return web page
+			CURLOPT_HEADER         => true, 	// return headers in addition to content
+			CURLOPT_FOLLOWLOCATION => true, 	// follow redirects
+			CURLOPT_ENCODING       => "", 		// handle all encodings
+			CURLOPT_AUTOREFERER    => true, 	// set referer on redirect
+			CURLOPT_CONNECTTIMEOUT => 120, 		// timeout on connect
+			CURLOPT_TIMEOUT        => 120, 		// timeout on response
+			CURLOPT_MAXREDIRS      => 10, 		// stop after 10 redirects
+			CURLINFO_HEADER_OUT    => true,
+			CURLOPT_SSL_VERIFYPEER => false, 	// Disabled SSL Cert checks
+			CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+			CURLOPT_COOKIE         => null,
+            CURLOPT_USERAGENT      => $user_agent[array_rand($user_agent)],
+            CURLOPT_HTTPHEADER     => ['Content-Type: text/plain; charset=UTF-8'],
+        ];
+        
+        $ch = curl_init( $url );
+		curl_setopt_array( $ch, $options );
+        $content = curl_exec( $ch );
+        curl_close($ch);
+
+        return mb_convert_encoding($content, 'utf-8', 'gb2312');
     }
 }
